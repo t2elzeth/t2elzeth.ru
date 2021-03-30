00e2f7385be5717f31c3930e616cc71a5f3e20e7
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-03-30 10:54:21 +0600

    Update

--- main_backend/apps/ar/send.py
+++ main_backend/apps/ar/send.py
@@ -1,12 +1,12 @@
 import pika
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-
 
 def send_new_project():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
     channel.basic_publish(exchange='',
                           routing_key='hello',
                           body=json.dumps({'event': 'new-project'}).encode())
+    connection.close()
