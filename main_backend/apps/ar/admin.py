22b81c722bf0f1a9649cf6364b72fa7683b687ae
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-04-03 21:32:28 +0600

    Update

--- main_backend/apps/ar/admin.py
+++ main_backend/apps/ar/admin.py
@@ -6,6 +6,9 @@ from .models import AR, ARAdmin as ARAdminModel
 class ARAdmin(admin.ModelAdmin):
     readonly_fields = ('id', 'image')
 
+    def has_add_permission(self, request):
+        return False
+
 
 admin.site.register(AR, ARAdmin)
 admin.site.register(ARAdminModel)
